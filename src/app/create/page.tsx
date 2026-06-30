91127c2c2f43fb5427219c04f2f552227f26bd3c
DAntonenko <dm.antonenko1977@gmail.com>
2026-06-30 19:19:34 +0400

    feat: banner in create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -5,6 +5,7 @@ import { useMemo, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import Banner from '@/components/ui/banner';
 import Image from 'next/image';
 
 import { JobFormData } from '@/store/job-form.store';
@@ -78,135 +79,140 @@ export default function Create() {
       </p>
     </main>
   ) : (
-    <main className="mt-8 flex w-full flex-col gap-8 sm:flex-row">
-      <section className="flex-1">
-        <h1 className="text-gray text-4xl font-semibold">
-          {form.jobTitle && form.company
-            ? `${form.jobTitle}, ${form.company}`
-            : 'New Application'}
-        </h1>
-        <hr className="bg-gray my-4 h-px w-full" />
-        <div className="space-y-6">
-          <div className="flex flex-col gap-4 md:flex-row">
-            <div className="flex flex-1 flex-col gap-2">
+    <main className="mt-8 flex w-full flex-col gap-8">
+      <div className="flex w-full flex-col gap-8 sm:flex-row">
+        <section className="flex-1">
+          <h1 className="text-gray text-4xl font-semibold">
+            {form.jobTitle && form.company
+              ? `${form.jobTitle}, ${form.company}`
+              : 'New Application'}
+          </h1>
+          <hr className="bg-gray my-4 h-px w-full" />
+          <div className="space-y-6">
+            <div className="flex flex-col gap-4 md:flex-row">
+              <div className="flex flex-1 flex-col gap-2">
+                <label className="text-gray-dark text-sm font-medium">
+                  Job title
+                </label>
+
+                <Input
+                  value={form.jobTitle}
+                  maxLength={100}
+                  placeholder="Product Manager"
+                  className="border-gray-light focus-visible:ring-gray-light"
+                  onChange={(e) =>
+                    handleFieldChange('jobTitle', e.target.value)
+                  }
+                />
+              </div>
+
+              <div className="flex flex-1 flex-col gap-2">
+                <label className="text-gray-dark text-sm font-medium">
+                  Company
+                </label>
+
+                <Input
+                  value={form.company}
+                  maxLength={100}
+                  placeholder="Apple"
+                  className="border-gray-light focus-visible:ring-gray-light"
+                  onChange={(e) => handleFieldChange('company', e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
               <label className="text-gray-dark text-sm font-medium">
-                Job title
+                I am good at...
               </label>
 
               <Input
-                value={form.jobTitle}
+                value={form.skills}
                 maxLength={100}
-                placeholder="Product Manager"
+                placeholder="HTML, CSS and doing things in time"
                 className="border-gray-light focus-visible:ring-gray-light"
-                onChange={(e) => handleFieldChange('jobTitle', e.target.value)}
+                onChange={(e) => handleFieldChange('skills', e.target.value)}
               />
             </div>
 
-            <div className="flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-2">
               <label className="text-gray-dark text-sm font-medium">
-                Company
+                Additional details
               </label>
 
-              <Input
-                value={form.company}
-                maxLength={100}
-                placeholder="Apple"
+              <Textarea
+                value={form.additionalDetails}
+                maxLength={1200}
+                rows={8}
+                placeholder="Describe why you are a great fit or paste your bio"
                 className="border-gray-light focus-visible:ring-gray-light"
-                onChange={(e) => handleFieldChange('company', e.target.value)}
+                onChange={(e) =>
+                  handleFieldChange('additionalDetails', e.target.value)
+                }
               />
+              <span
+                className={`text-xs ${
+                  form.additionalDetails.length > 1000
+                    ? 'text-red-500'
+                    : 'text-muted-foreground'
+                }`}
+              >
+                {form.additionalDetails.length}/1200
+              </span>
             </div>
+
+            {generatedText ? (
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => setGeneratedText(null)}
+              >
+                <Image
+                  src="/icons/repeat.svg"
+                  alt="refresh icon"
+                  width={24}
+                  height={24}
+                />
+                Try Again
+              </Button>
+            ) : (
+              <Button
+                disabled={!canGenerate}
+                onClick={handleGenerate}
+                className="w-full"
+              >
+                Generate Now
+              </Button>
+            )}
           </div>
-
-          <div className="flex flex-col gap-2">
-            <label className="text-gray-dark text-sm font-medium">
-              I am good at...
-            </label>
-
-            <Input
-              value={form.skills}
-              maxLength={100}
-              placeholder="HTML, CSS and doing things in time"
-              className="border-gray-light focus-visible:ring-gray-light"
-              onChange={(e) => handleFieldChange('skills', e.target.value)}
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label className="text-gray-dark text-sm font-medium">
-              Additional details
-            </label>
-
-            <Textarea
-              value={form.additionalDetails}
-              maxLength={1200}
-              rows={8}
-              placeholder="Describe why you are a great fit or paste your bio"
-              className="border-gray-light focus-visible:ring-gray-light"
-              onChange={(e) =>
-                handleFieldChange('additionalDetails', e.target.value)
-              }
-            />
-            <span
-              className={`text-xs ${
-                form.additionalDetails.length > 1000
-                  ? 'text-red-500'
-                  : 'text-muted-foreground'
-              }`}
-            >
-              {form.additionalDetails.length}/1200
-            </span>
-          </div>
-
+        </section>
+        <section className="bg-gray-lighter flex flex-1 flex-col justify-between rounded-md p-6">
           {generatedText ? (
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={() => setGeneratedText(null)}
-            >
-              <Image
-                src="/icons/repeat.svg"
-                alt="refresh icon"
-                width={24}
-                height={24}
-              />
-              Try Again
-            </Button>
+            <p className="whitespace-pre-wrap">{generatedText}</p>
           ) : (
-            <Button
-              disabled={!canGenerate}
-              onClick={handleGenerate}
-              className="w-full"
-            >
-              Generate Now
-            </Button>
+            <p>Your personalized job application will appear here...</p>
           )}
-        </div>
-      </section>
-      <section className="bg-gray-lighter flex flex-1 flex-col justify-between rounded-md p-6">
-        {generatedText ? (
-          <p className="whitespace-pre-wrap">{generatedText}</p>
-        ) : (
-          <p>Your personalized job application will appear here...</p>
-        )}
-        <footer className="flex items-center justify-end">
-          <Button
-            variant="ghost"
-            size="gh"
-            disabled={!generatedText}
-            onClick={
-              generatedText ? () => copyToClipboard(generatedText) : undefined
-            }
-          >
-            Copy to Clipboard
-            <Image
-              src="/icons/copy.svg"
-              alt="copy icon"
-              width={20}
-              height={20}
-            />
-          </Button>
-        </footer>
-      </section>
+          <footer className="flex items-center justify-end">
+            <Button
+              variant="ghost"
+              size="gh"
+              disabled={!generatedText}
+              onClick={
+                generatedText ? () => copyToClipboard(generatedText) : undefined
+              }
+            >
+              Copy to Clipboard
+              <Image
+                src="/icons/copy.svg"
+                alt="copy icon"
+                width={20}
+                height={20}
+              />
+            </Button>
+          </footer>
+        </section>
+      </div>
+      {applicationsCount < MAX_APPLICATIONS && <Banner />}
     </main>
   );
 }
